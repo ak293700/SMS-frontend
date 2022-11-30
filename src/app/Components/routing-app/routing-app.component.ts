7c0b83c5511f12414e1df7cb3b2829212b1988e7
alexandreakdeniz <alexandre.akdeniz@free.fr>
2022-11-30 16:40:01 +0100

    add two first component.

---
+++ src/app/Components/routing-app/routing-app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-routing-app',
+  templateUrl: './routing-app.component.html',
+  styleUrls: ['./routing-app.component.css']
+})
+export class RoutingAppComponent
+{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ButtonModule} from "primeng/button";
+import {RoutingAppComponent} from './Components/routing-app/routing-app.component';
+import {LoginPageComponent} from './Components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingAppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RoutingAppComponent]
 })
 export class AppModule { }
