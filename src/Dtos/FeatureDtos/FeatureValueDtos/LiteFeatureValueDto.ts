51594d690b5d199b55041c3ce3b6beb781fbf219
alexandreakdeniz <alexandre.akdeniz@free.fr>
2023-01-11 17:17:07 +0100

    start predefined-key-value-table.component. Should do another component that uses this one and to specifically handle the feature.

---
+++ src/Dtos/FeatureDtos/FeatureValueDtos/LiteFeatureValueDto.ts
@@ -0,0 +1,5 @@
+export interface LiteFeatureValueDto
+{
+    id: number;
+    value: string;
+}
--- src/app/Components/selectors/predefined-key-value-table/predefined-key-value-table.component.ts
+++ src/app/Components/selectors/predefined-key-value-table/predefined-key-value-table.component.ts
@@ -1,4 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IdNameDto} from "../../../../Dtos/IdNameDto";
+import {FeatureModelsService} from "../../../Services/feature-model.service.ts.service";
+import {LiteFeatureValueDto} from "../../../../Dtos/FeatureDtos/FeatureValueDtos/LiteFeatureValueDto";
+import {Operation} from "../../../../utils/Operation";
 
 /*
 * Ce composant permet d'éditer (ajouter, supprimer, éditer, ...) une collection de clé/valeur prédéfinie.
@@ -12,7 +16,74 @@ import {Component} from '@angular/core';
         './predefined-key-value-table.component.css'
     ]
 })
-export class PredefinedKeyValueTableComponent
+export class PredefinedKeyValueTableComponent implements OnInit
 {
 
+    initialKeySuggestions: IdNameDto[] = [];
+    keySuggestions: IdNameDto[] = this.initialKeySuggestions;
+
+    // for each model we have a list of values
+    private _valueSuggestions: { [key: number]: IdNameDto[] } = {};
+
+    // we can have only one value suggestion because
+    // the user click only on one dropdown at the time
+    valueSuggestion: IdNameDto[] = [];
+
+    currentFeatures: {
+        model: IdNameDto,
+        value: LiteFeatureValueDto
+    }[] = [];
+
+    constructor(private featureModelsService: FeatureModelsService)
+    {
+        this.initialKeySuggestions = [
+            {id: 1, name: "Couleur"},
+            {id: 2, name: "Expédié sous"},
+        ];
+
+        this.keySuggestions = Operation.deepCopy(this.initialKeySuggestions);
+
+        this.currentFeatures = [
+            {
+                model: {
+                    id: 1,
+                    name: "Couleur"
+                },
+                value: {
+                    id: 1,
+                    value: "Rouge"
+                }
+            },
+            {
+                model: {
+                    id: 1,
+                    name: "Expédié sous"
+                },
+                value: {
+                    id: 1,
+                    value: "24/48h"
+                }
+            }
+        ];
+    }
+
+    async ngOnInit()
+    {
+        this.keySuggestions = await this.featureModelsService.getFeatureModels();
+    }
+
+    createEmptyValue()
+    {
+
+    }
+
+    deleteValue(featureValue: any)
+    {
+
+    }
+
+    completeMethodForModel(event: any)
+    {
+        this.keySuggestions = Operation.completeMethod(event.query, this.initialKeySuggestions);
+    }
 }
